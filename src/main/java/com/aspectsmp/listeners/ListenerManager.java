672ca2249b754ced872cd7d343dd15351941793f
Aaryadev <aaryadev@example.com>
2026-07-06 16:12:40 +1000

    fix: add missing Player import in ListenerManager

--- src/main/java/com/aspectsmp/listeners/ListenerManager.java
+++ src/main/java/com/aspectsmp/listeners/ListenerManager.java
@@ -7,6 +7,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.EquipmentSlot;
