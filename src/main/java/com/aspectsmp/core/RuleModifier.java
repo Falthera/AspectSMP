b9ce644703b7eb9b7806599d0fc016a7805151dd
Aaryadev <aaryadev@example.com>
2026-07-10 15:37:01 +1000

    fix: properly remove modifyKnockback from RuleModifier interface

--- src/main/java/com/aspectsmp/core/RuleModifier.java
+++ src/main/java/com/aspectsmp/core/RuleModifier.java
@@ -6,9 +6,12 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import java.util.UUID;
 
 public interface RuleModifier {
+
     double modifyDamage(Player player, double damage, EntityDamageEvent.DamageCause cause);
+
     void modifyMovement(Player player);
-    double modifyKnockback(Player player, double knockback);
+
     void applyPassive(Player player);
+
     default void recordCombat(UUID uuid) {}
 }
